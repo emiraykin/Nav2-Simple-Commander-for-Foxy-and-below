6c1e7c7469130765ae841f5a4764e8ef411366e2
yutanakamura01 <yutanakamura01@gmail.com>
2023-10-05 14:55:26 +0900

    classの追加、csvファイルからwaypointを読み込み機能を追加、３秒に一回残り距離を表示する、actionserverから状態を受け取るcallbackの追加、読み込むcsvの例を追加

--- simple_commander_for_foxy/simple_commander.py
+++ simple_commander_for_foxy/simple_commander.py
@@ -1,79 +1,86 @@
-
 import rclpy
 from rclpy.action import ActionClient
-from geometry_msgs.msg import PoseStamped
+from rclpy.node import Node
 from nav2_msgs.action import NavigateToPose
-#author: emiraykin
+from geometry_msgs.msg import PoseStamped
+import csv
+import time
 
+class WaypointSender(Node):
+    def __init__(self):
+        super().__init__('waypoint_sender')
+        self._action_client = ActionClient(self, NavigateToPose, 'navigate_to_pose')
+        self.waypoints = self.load_waypoints_from_csv('output.csv')
+        self.current_waypoint_index = 0
+        self._last_feedback_time = self.get_clock().now()
 
+    def load_waypoints_from_csv(self, filename):
+        waypoints = []
+        with open(filename, mode='r') as file:
+            reader = csv.reader(file)
+            header = next(reader)
+            for row in reader:
+                pose_stamped_msg = PoseStamped()
+                pose_stamped_msg.header.frame_id = 'map'  # Adjust the frame_id as needed
+                pose_stamped_msg.pose.position.x = float(row[1])
+                pose_stamped_msg.pose.position.y = float(row[2])
+                pose_stamped_msg.pose.position.z = float(row[3])
+                pose_stamped_msg.pose.orientation.x = float(row[4])
+                pose_stamped_msg.pose.orientation.y = float(row[5])
+                pose_stamped_msg.pose.orientation.z = float(row[6])
+                pose_stamped_msg.pose.orientation.w = float(row[7])
+                waypoints.append(pose_stamped_msg)
+        return waypoints
 
-# This script only can be used for adding navigation goals 
-
-# For setting initial pose you can have a look here:
-# https://answers.ros.org/question/392682/how-to-use-nav2_simple_commander-in-foxy/
-
-
-def main():
-    rclpy.init()
-
-    # Create a ROS 2 node
-    node = rclpy.create_node('simple_commander')
-
-    # Create an action client called "navigate_to_pose" with action definition "NavigateToPose"
-    client = ActionClient(node, NavigateToPose, 'navigate_to_pose')
-
-    # Wait until the action server has started up and started listening for goals
-    while not client.wait_for_server(timeout_sec=3.0):
-        print('Action server not available!')
-        #rclpy.shutdown()
-        #return
-
-    # Define a list of goal coordinates
-    goal_coordinates = [
-        (2.0, 0.0, 0.0),  # x, y, theta for Goal 1
-        (0.0, 2.0, 1.57),  # x, y, theta for Goal 2
-        (-2.0, -0.5, 0.0),  # x, y, theta for Goal 3
-    ]
-
-    # Loop over the goal coordinates and send goals one by one
-    #while True:
-    for goal_x, goal_y, goal_theta in goal_coordinates:
-        # Create a goal message with the PoseStamped message
+    def send_goal(self, pose_stamped):
         goal_msg = NavigateToPose.Goal()
+        goal_msg.pose = pose_stamped
+        
+        while not self._action_client.wait_for_server(timeout_sec=1.0):
+            self.get_logger().warn('Action server not available, waiting...')
+        
+        self.get_logger().info('Sending waypoint...')
+        send_goal_future = self._action_client.send_goal_async(goal_msg, feedback_callback=self.feedback_callback)
+        send_goal_future.add_done_callback(self.goal_response_callback)
 
-        # Set the goal target pose
-        goal_msg.pose.header.frame_id = 'map'
-        goal_msg.pose.pose.position.x = goal_x
-        goal_msg.pose.pose.position.y = goal_y
-        goal_msg.pose.pose.orientation.z = goal_theta
+    def feedback_callback(self, feedback_msg):
+        current_time = self.get_clock().now()
+        if (current_time - self._last_feedback_time).nanoseconds >= 3e9:
+            self.get_logger().info('Received feedback: {0}'.format(feedback_msg.feedback.distance_remaining))
+            self._last_feedback_time = current_time
 
-        # Send the goal to the action server
-        goal_handle_future = client.send_goal_async(goal_msg)
+    def goal_response_callback(self, future):
+        goal_handle = future.result()
+        if not goal_handle.accepted:
+            self.get_logger().warn('Goal rejected by server')
+            # Resend the goal or handle accordingly
+            # ...
 
-        #print("id: " + str(i))
+        goal_handle.get_result_async().add_done_callback(self.goal_result_callback)
+        
+    def goal_result_callback(self, future):
+        result = future.result().result
+        self.get_logger().info('Goal completed with result: {0}'.format(result))
+        self.current_waypoint_index += 1
+        
+        if self.current_waypoint_index < len(self.waypoints):
+            next_waypoint = self.waypoints[self.current_waypoint_index]
+            next_waypoint.header.stamp = self.get_clock().now().to_msg()
+            self.send_goal(next_waypoint)
 
-        # Wait for the goal to complete
-        while rclpy.ok():
-            rclpy.spin_once(node)
-            if goal_handle_future.done():
-                goal_handle = goal_handle_future.result()
-                if goal_handle.accepted:
-                    print('Goal accepted.')
-                    result_future = goal_handle.get_result_async()
-                    rclpy.spin_until_future_complete(node, result_future)
-                    result = result_future.result().result
-                    if result!=False:
-                        print('Goal execution done!')
-                    else:
-                        print('Goal execution failed!')
-                    break
-                else:
-                    print('Goal rejected.')
-                    #break #単純に次のポイントに進もうとすると別のソフトウェアのつなぎで断続してrejectedになる
+    def run(self):
+        if self.waypoints:
+            self.send_goal(self.waypoints[self.current_waypoint_index])
 
+def main(args=None):
+    rclpy.init(args=args)
 
-    print("Task Competed!")
-    # Clean up resources
+    waypoint_sender = WaypointSender()
+    waypoint_sender.run()
+    
+    rclpy.spin(waypoint_sender)
+
+    waypoint_sender.destroy_node()
     rclpy.shutdown()
 
 if __name__ == '__main__':
